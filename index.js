f3dba97ce1231fc6a646a5d96ce8999b031825bb
vinxayak-xii <78162504+vinxayak-xii@users.noreply.github.com>
2021-09-06 01:43:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ const { request } = require('http');
 require("dotenv").config();
 
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.listen(3000, ()=> console.log('listening at 3000'));
+app.listen(port, ()=> console.log('listening at 3000'));
 app.use(express.static('public'));
 app.use(express.json({limit: '1mb'}));
 
